7bcec61ed2d935e453bc93328620f201c621eaea
Cheng-Nan Liu <Cheng-Nan.Liu@utah.edu>
2026-05-22 10:25:41 -0600

    Bulk sync: 10 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-18.py
@@ -0,0 +1,28 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        # nums_dict = {}
+        # # target = 0
+        # # res = False
+        # for i, n in enumerate(nums):
+        #     # diff = target - n
+        #     if n in nums_dict:
+        #         return True
+        #     nums_dict[n] = i
+        # return False
+        # res = False
+        # nums.sort()
+        # l, r = 0, len(nums) -1
+        # while r > l:
+        #     if nums[l] < nums[r]:
+        #         r -= 1
+        #     elif nums[l] == nums[r]:
+        #         res =  True
+        #     else:
+        #         res = False
+        # return res
+        ###=== using set===###
+        new_nums = set(nums)
+        if len(nums) != len(new_nums):
+            return True
+        else:
+            return False
