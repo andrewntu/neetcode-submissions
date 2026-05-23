e19d369802c91040799da832069d80e6a3686452
Cheng-Nan Liu <Cheng-Nan.Liu@utah.edu>
2026-05-23 11:26:53 -0600

    Add: buy-and-sell-crypto - submission-9

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-9.py
@@ -0,0 +1,25 @@
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         l, r = 0, 1
+#         res = 0
+#         while l < r and r < len(prices):
+#             if prices[r] > prices[l]:
+#                 res = max(res, prices[r] - prices[l])
+#             else:
+#                 l = r
+#             r += 1
+#         return res
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = prices[0]
+        res = 0
+
+        for i, price in enumerate(prices):
+            if price < min_price:
+                min_price = price
+
+            profit = price - min_price
+            res = max(res, profit)
+
+        return res
