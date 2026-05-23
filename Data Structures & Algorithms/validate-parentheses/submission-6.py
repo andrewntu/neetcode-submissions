3064b5a79f35a954d883ff690de359ef4000f2b9
Cheng-Nan Liu <Cheng-Nan.Liu@utah.edu>
2026-05-23 10:50:23 -0600

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        para = {")": "(", "]": "[", "}": "{"}
+        stack = []
+
+        for c in s:
+            if c in para:
+                if not stack or stack[-1] is not para[c]:
+                    return False
+                else: 
+                    stack.pop()
+            else:    
+                stack.append(c)
+        return len(stack) == 0
